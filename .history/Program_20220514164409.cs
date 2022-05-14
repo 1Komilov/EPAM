f3e72ade9c223dcd700988124f4bb68ccf4acf9e
macbook pro <macbookpro@Komilv.local>
2022-05-14 16:48:48 +0500

    first commit

---
+++ .history/Program_20220514164409.cs
@@ -0,0 +1,29 @@
+﻿namespace World; 
+ 
+public class Program  
+{          
+    public static void Main(string[] args) 
+    { 
+        Console.WriteLine("Enter any 2 numbers : ");      
+        double i,fact=1;       
+        Console.Write("Enter a : "); 
+        double a = Convert.ToDouble(Console.ReadLine()); 
+        Console.Write("Enter b : "); 
+        double b = Convert.ToDouble(Console.ReadLine());      
+        for(i = 1;i <= a;i++) 
+        {       
+            fact = fact * i;       
+        }       
+      
+        if (fact2 > log) 
+        { 
+            Console.WriteLine($"Number a = {fact2:0.00} and more than number  b = {log:0.00}"); 
+             
+        } 
+        else 
+        { 
+            Console.WriteLine($"Number b = {log:0.00} and more than number a = {fact2:0.00}"); 
+        } 
+ 
+    } 
+}
